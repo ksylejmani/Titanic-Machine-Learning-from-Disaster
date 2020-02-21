99998d8e8d0def88b6db1536cbb6a8dec6a58b3f
Kadri Sylejmani <kadri.sylejmani@uni-pr.edu>
2020-02-21 16:48:39 +0100

    Added xgboost model

--- xgboost_model.py
+++ xgboost_model.py
@@ -1,22 +1,27 @@
+from xgboost import XGBClassifier
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 
+# *** Importing data for the project of Titanic: Machine Learning from Disaster
 train_file_path = 'input/train.csv'
 test_file_path = 'input/test.csv'
 X = pd.read_csv(train_file_path)
 X_test = pd.read_csv(test_file_path)
 
-# Remove rows with missing target, separate target from predictors
+# *** Preparing target column: remove rows with missing target, separate target from predictors
 X.dropna(axis=0, subset=['Survived'], inplace=True)
 y = X.Survived
 X.drop(['Survived'], axis=1, inplace=True)
 
+# *** Converting columns with date-time types (i.e. timestamps)
+# this project does not have date-time types
+
+# Prepping categorical variables
 categorical_columns = [col for col in X if X[col].dtype == 'object']
 numerical_columns = [col for col in X if X[col].dtype in ['int32', 'int64', 'float64']]
 
@@ -36,11 +41,11 @@ preprocessor = ColumnTransformer(
         ('cat', categorical_transformer, categorical_columns)
     ])
 
-# Divide data into training and validation subsets
+# *** Creating training, validation, and test splits
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
 
-# Create the model
-model = RandomForestClassifier(n_estimators=30000, random_state=0,max_depth=20)
+# *** Training a evaluating the model
+model = XGBClassifier(num_leaves=64, n_estimators=5000, learning_rate=0.001, min_child_samples=10)
 # Bundle preprocessor and modeling code in a pipeline
 titanic_pipeline = Pipeline(steps=[('preprocessor', preprocessor), ('model', model)])
 
@@ -54,7 +59,7 @@ predictions_valid = titanic_pipeline.predict(X_valid)
 score = roc_auc_score(y_valid, predictions_valid)
 print("roc_auc_score: ", score)
 
-# Make predictions using test data for submission to competition
+# *** Applying the model - make predictions using test data for submission to competition
 predictions = titanic_pipeline.predict(X_test)
 output = pd.DataFrame({'PassengerId': X_test.PassengerId, 'Survived': predictions})
 output.to_csv('output/submission.csv', index=False)
